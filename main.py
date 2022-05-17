b9f8fa7f360b8c93d0bd9059c66d8e9370652255
lucacm <lucacm@al.insper.edu.br>
2022-05-17 00:02:07 -0300

    upload main.py final

--- main.py
+++ main.py
@@ -1,161 +1,101 @@
 from socket import create_server
 from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 from sympy import product
 from fastapi.encoders import jsonable_encoder
+from db import connection
+from sqlalchemy import Column, Table
+from sqlalchemy.sql.sqltypes import Integer, String
+from db import meta, engine
+from starlette.status import HTTP_204_NO_CONTENT
 
+# nome da database --> shopping
 app = FastAPI()
 
-market = {
-    "carts":[
-        {"id_cart" : 1, "id_user_fk" : 1, "product_fk_id" : []},
-        {"id_cart" : 2, "id_user_fk" : 2, "product_fk_id" : []},
-        {"id_cart" : 3, "id_user_fk" : 3, "product_fk_id" : []}
-    ],
-    "products":[
-        {"id_prod" : 1, "name_prod" : "Melão", "description_prod" : "Fruta", "price_prod" : 6.5, "tax_prod" : 3.2},
-        {"id_prod" : 2, "name_prod" : "Presunto", "description_prod" : "Frios", "price_prod" : 10.0, "tax_prod" : 2.0},
-        {"id_prod" : 3, "name_prod" : "Papel Toalha", "description_prod" : "Limpeza", "price_prod" : 3.79, "tax_prod" : 4.5}
-    ]
-}
+produto = Table("produto", meta, Column("id_prod", Integer, primary_key=True), Column(
+    "name_prod", String(255),), Column("description_prod", String(255)), Column("price_prod", String(255)), Column("tax_prod", String(255)))
+
+carrinho = Table("carrinho", meta, Column("id_cart", Integer, primary_key=True), Column(
+    "id_user_fk", String(255)), Column("produtos", String(255)))
+
+meta.create_all(engine)
+
 
 class Product_Add(BaseModel):
-    id_prod: Optional[int] = None
+    id_prod: Optional[int]
     name_prod: str
-    description_prod: Optional[str] = None
-    price_prod: float
-    tax_prod: Optional[float] = None
+    description_prod: Optional[str]
+    price_prod: str
+    tax_prod: str
+
 
 class Cart_Add(BaseModel):
-    id_cart: Optional[int] = None
+    id_cart: Optional[int]
     id_user_fk: int
-    product_fk_id: list = []
-    
-## ---------- PRODUTO ----------
-@app.post("/products/", tags=["Produto"])
-async def create_product(product: Product_Add):
-    max_id = 0
-    for item in market["products"]:
-        if max_id < item["id_prod"]:
-            max_id = item["id_prod"]
-    max_id += 1
-    product.id_prod = max_id
-    update_product_encoded = jsonable_encoder(product)
-    market["products"].append(update_product_encoded)
-    return {
-        "status": True,
-        "message": "Product has been added successfully."
-    }
+    produtos: str
+
+
+# ---------- PRODUTO ----------
+
+@app.get("/products", tags=["Produto"])
+async def read_products():
+    return connection.execute(produto.select()).fetchall()
+
 
 @app.get("/products/{product_id}", tags=["Produto"])
 async def read_product(product_id: int):
-    for items in market["products"]:
-        if items["id_prod"] == product_id:
-            return items
-    return {
-        "status": False,
-        "message": "Product was not found."
-    }
+    return connection.execute(produto.select().where(produto.c.id_prod == product_id)).first()
 
-@app.delete("/products/{product_id}", tags=["Produto"])
-async def delete_item(product_id:int):
-    for items in market["products"]:
-        if items["id_prod"] == product_id:
-            market["products"].remove(items)
-            return {
-                "status": True,
-                "message": "Product was deleted successfully."
-            }
-    return {
-        "status": False,
-        "message": "Product was not found."
-    }
+
+@app.post("/products/add", tags=["Produto"])
+async def create_product(product: Product_Add):
+    new_product = {"name_prod": product.name_prod, "description_prod": product.description_prod,
+                   "price_prod": product.price_prod, "tax_prod": product.tax_prod}
+    result = connection.execute(produto.insert().values(new_product))
+    return connection.execute(produto.select().where(produto.c.id_prod == result.lastrowid)).first()
+
 
 @app.put("/products/{product_id}", tags=["Produto"])
-async def update_item(product_id:int, product: Product_Add):
-    for items in market["products"]:
-        if items["id_prod"] == product_id:
-            market["products"].remove(items)
-            product.id_prod = product_id
-            items = jsonable_encoder(product)
-            print(items)
-            market["products"].append(items)
-            return {
-                "status": True,
-                "message": "Product was updated successfully."
-            }
-    return {
-        "status": False,
-        "message": "Product was not found."
-    }
+async def update_item(product_id: int, product: Product_Add):
+    connection.execute(produto.update().values(name_prod=product.name_prod, description_prod=product.description_prod,
+                       price_prod=product.price_prod, tax_prod=product.tax_prod).where(produto.c.id_prod == product_id))
+    return connection.execute(produto.select().where(produto.c.id_prod == product_id)).first()
 
-## ---------- CARRINHO ----------
-@app.post("/carts/", tags=["Carrinho"])
-async def create_cart(cart: Cart_Add):
-    max_id = 0
-    for item in market["carts"]:
-        if max_id < item["id_cart"]:
-            max_id = item["id_cart"]
-    max_id += 1
-    cart.id_cart = max_id
-    update_cart_encoded = jsonable_encoder(cart)
-    market["carts"].append(update_cart_encoded)
-    return {
-        "status": True,
-        "message": "Cart has been created successfully."
-    }
+
+@app.delete("/products/{product_id}", tags=["Produto"], status_code=HTTP_204_NO_CONTENT)
+async def delete_item(product_id: int):
+    connection.execute(produto.delete().where(produto.c.id_prod == product_id))
+    return connection.execute(produto.select().where(produto.c.id_prod == product_id)).first()
+
+
+# ---------- CARRINHO ----------
 
 @app.get("/carts/", tags=["Carrinho"])
 async def read_carts():
-    return market["carts"]
+    return connection.execute(carrinho.select()).fetchall()
+
 
 @app.get("/carts/{carts_id}", tags=["Carrinho"])
 async def read_cart(cart_id: int):
-    for cart in market["carts"]:
-        if cart["id_cart"] == cart_id:
-            return cart
-    return {
-        "status": False,
-        "message": "Cart was not found."
-    }
+    return connection.execute(carrinho.select().where(carrinho.c.id_cart == cart_id)).first()
 
-@app.delete("/carts/{carts_id}", tags=["Carrinho"])
-async def delete_cart(cart_id:int):
-    for cart in market["carts"]:
-        if cart["id_cart"] == cart_id:
-            market["carts"].remove(cart)
-            return {
-                "status": True,
-                "message": "Cart was deleted successfully."
-            }
-    return {
-        "status": False,
-        "message": "Cart was not found."
-    }
+
+@app.post("/carts/add", tags=["Carrinho"])
+async def create_cart(cart: Cart_Add):
+    new_product = {"id_user_fk": cart.id_user_fk, "produtos": cart.produtos}
+    result = connection.execute(carrinho.insert().values(new_product))
+    return connection.execute(carrinho.select().where(carrinho.c.id_cart == result.lastrowid)).first()
+ 
 
 @app.put("/carts/{carts_id}", tags=["Carrinho"])
-async def update_item(cart_id:int, cart: Cart_Add):
-    product_id_list = market["products"]
-    for items in market["carts"]:
-        if items["id_cart"] == cart_id:
-            market["carts"].remove(items)
-            cart.id_cart = cart_id
-            cart_id_list = cart.product_fk_id
-            cart.product_fk_id = []
-            for list_id in cart_id_list:
-                for i in product_id_list:
-                    if i["id_prod"] == list_id:
-                        print(i["id_prod"])
-                        cart.product_fk_id.append(i["id_prod"])
-                        break
-            items = jsonable_encoder(cart)
-            market["carts"].append(items)
-            return {
-                "status": True,
-                "message": "Cart was updated successfully."
-            }
-    return {
-        "status": False,
-        "message": "Cart was not found."
-    }
+async def update_item(cart_id: int, cart: Cart_Add):
+    connection.execute(carrinho.update().values(
+        id_user_fk=cart.id_user_fk, produtos=cart.produtos).where(carrinho.c.id_cart == cart_id))
+    return connection.execute(carrinho.select().where(carrinho.c.id_cart == cart_id)).first()
+
+
+@app.delete("/carts/{carts_id}", tags=["Carrinho"], status_code=HTTP_204_NO_CONTENT)
+async def delete_cart(cart_id: int):
+    connection.execute(carrinho.delete().where(carrinho.c.id_cart == cart_id))
+    return connection.execute(carrinho.select().where(carrinho.c.id_cart == cart_id)).first()
